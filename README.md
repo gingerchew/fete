20c02f2399c1c0df643800d96dab320ea42e4329
ginger <26461046+gingerchew@users.noreply.github.com>
2023-10-20 14:47:16 -0400

    Updates the package size
    based on count from bitesize.land

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 > Feast, festival, celebration
 
-Juhla is a wrapper around the native Event Target interface. It clocks in at around 247b (166b gzip, 140b br), and uses familiar methods.
+Juhla is a wrapper around the native Event Target interface. It clocks in at around 290b (195b gzip, 168b br), and uses familiar methods.
 
 ```js
 import { juhla } from 'juhla'; // notice it is juhla and not fête
--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,19 @@
-let d = (o = "", v = new EventTarget()) => new Proxy({ emit(e, n) {
-  v.dispatchEvent(new CustomEvent(o + e, n));
-}, on(e, n, t) {
-  v.addEventListener(o + e, n, t);
-}, off(e, n, t) {
-  v.removeEventListener(o + e, n, t);
-}, one(e, n, t = {}) {
-  t.once = !0, v.addEventListener(o + e, n, t);
-} }, { get: (e, n) => e[n] ?? ((t, E) => e.on(n, t, E)) });
+let d = (o = "", v = new EventTarget()) => new Proxy({
+  emit(e, n) {
+    v.dispatchEvent(new CustomEvent(o + e, n));
+  },
+  on(e, n, t) {
+    v.addEventListener(o + e, n, t);
+  },
+  off(e, n, t) {
+    v.removeEventListener(o + e, n, t);
+  },
+  one(e, n, t = {}) {
+    t.once = !0, v.addEventListener(o + e, n, t);
+  }
+}, {
+  get: (e, n) => e[n] ?? ((t, E) => e.on(n, t, E))
+});
 export {
   d as juhla
 };
